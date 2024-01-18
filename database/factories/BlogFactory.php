47883eca2c40ee6abd4ff2cbe0c56f062939753d
jean luc kawel <20ka208@esisalama.org>
2024-01-18 14:16:10 +0200

    first commit

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->word(),
+            'content' => $this->faker->word(),
+            'picture' => $this->faker->imageUrl('100'),
+            'like' => $this->faker->randomDigit(),
+            'dislike'=> $this->faker->randomDigit(),
+        ];
+    }
+}
---
+++ database/migrations/2024_01_18_115257_create_blogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('content', 100)->nullable();
+            $table->string('picture')->nullable();
+            $table->integer('like')->default('0');
+            $table->integer('dislike')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
